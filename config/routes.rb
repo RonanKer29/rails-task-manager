fb6f2864a50cebe92f27c4580f9b794eaba31b67
RonanKer29 <kervella.ronan@hotmail.com>
2024-02-20 14:20:57 +0100

    task list rails done

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,25 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
+
+  # read tasks
   get 'tasks', to: 'tasks#index'
+
+  # create tasks
   get 'tasks/new', to: 'tasks#new', as: :new_task
   post 'tasks', to: 'tasks#create'
+
+  # read tasks
   get 'tasks/:id', to: 'tasks#show', as: :task
+
+    # update tasks
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+    # delete task
+
+    delete "tasks/:id", to: "tasks#destroy"
+
   # root "posts#index"
 end
